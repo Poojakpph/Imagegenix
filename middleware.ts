5075b9b758db5ce1a47f540b72abefed8d6ce054
gituser <poojakpph.2001@gmail.com>
2024-09-18 14:12:39 +0530

    lastcommit

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,33 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+import { authMiddleware, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-import { authMiddleware, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
- 
-export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk']
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+
+// Define your authMiddleware configuration
+const auth = authMiddleware({
+  publicRoutes: ['/api/webhooks/clerk'],
 });
 
+// Define your clerkMiddleware configuration
+const clerk = clerkMiddleware({
+ 
+});
+
+export async function middleware(req: NextRequest, event: NextFetchEvent) {
+  // Apply Clerk middleware
+  const clerkResponse = await clerk(req, event);
+
+  // Apply auth middleware if the request is not public
+  if (!isPublicRoute(req)) {
+    return auth(req, event);
+  }
+
+  return clerkResponse;
+}
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
