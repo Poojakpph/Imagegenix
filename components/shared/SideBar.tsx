f2c390bc21548fcf44839b117eb500385344469d
Pooja Kumari <poojakpph.2001@gmail.com>
2024-10-03 01:32:01 +0530

    Update SideBar.tsx

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -55,7 +55,7 @@ const SideBar = () => {
           </SignedIn>
 
           <SignedOut>
-            <Button asChild className='button bg-purple-gradient bg-cover'>
+            <Button asChild className='button bg-cover'>
               <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
